ec867f08b4438e4b0fe73a21b99de34e745ddf0e
Doug Dellolio <doug.dellolio@gmail.com>
2017-02-26 16:04:06 -0500

    [Feature] More Pricing Data (#5)

--- YahooFinanceClient/YahooFinanceClient/CsvParser/CsvParser.cs
+++ YahooFinanceClient/YahooFinanceClient/CsvParser/CsvParser.cs
@@ -27,7 +27,7 @@ namespace YahooFinanceClient.CsvParser
 
         private PricingData RetrievePricingData(string ticker)
         {
-            var stockData = webClient.DownloadFile(ticker, "abb2b3po");
+            var stockData = webClient.DownloadFile(ticker, "abb2b3pokjj5k4j6k5w");
 
             var splitData = stockData.Split(',');
 
@@ -39,6 +39,13 @@ namespace YahooFinanceClient.CsvParser
                 BidRealTime = CleanData(splitData[3]),
                 PreviousClose = CleanData(splitData[4]),
                 Open = CleanData(splitData[5]),
+                FiftyTwoWeekHigh = CleanData(splitData[6]),
+                FiftyTwoWeekLow = CleanData(splitData[7]),
+                FiftyTwoWeekLowChange = CleanData(splitData[8]),
+                FiftyTwoWeekHighChange = CleanData(splitData[9]),
+                FiftyTwoWeekLowChangePercent = CleanPercent(splitData[10]),
+                FiftyTwoWeekHighChangePercent = CleanPercent(splitData[11]),
+                FiftyTwoWeekRange = splitData[12],
             };
         }
 
@@ -66,6 +73,24 @@ namespace YahooFinanceClient.CsvParser
             }
 
             return Convert.ToDecimal(data);
+        }
+
+        private decimal? CleanPercent(string data)
+        {
+            if (data == "N/A")
+            {
+                return null;
+            }
+
+            var direction = data.ToCharArray()[0];
+            var number = data.Substring(1, data.Length - 2);
+
+            if(direction == '-')
+            {
+                return -Convert.ToDecimal(number);
+            }
+
+            return Convert.ToDecimal(number);
         }
     }
 }
